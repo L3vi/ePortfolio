b54d4bdcfe7b97953a852628a0ea0b2d9bc29a82
L3vi <levistum@gmail.com>
2017-11-19 00:50:43 -0700

    So it begins.

---
+++ script.js
@@ -0,0 +1,31 @@
+//$(document).ready(function () {
+//
+//    $(function () {
+//        $("#header1").click(function () {
+//            $("#aboutMe").slideToggle(300);
+//            console.log(documentf.body.scrollHeight)
+//        });
+//    });
+//
+//    $(function () {
+//        $("#header2").click(function () {
+//            $("#experience").slideToggle(300);
+//            console.log(document.body.scrollHeight)
+//        });
+//    });
+//
+//    $(function () {
+//        $("#header3").click(function () {
+//            $("#projects").slideToggle(300);
+//            console.log(document.body.scrollHeight)
+//        });
+//    });
+//
+//    $(function () {
+//        $("#header4").click(function () {
+//            $("#contactMe").slideToggle(300);
+//            console.log(document.body.scrollHeight)
+//        });
+//    });
+//
+//});
